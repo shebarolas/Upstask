c0d675a03c1f2309b560fc8517b559299cf9b46a
shebarolas <30813367+shebarolas@users.noreply.github.com>
2022-03-16 14:42:40 -0300

    cambio de sequelize

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -1,4 +1,4 @@
-const Sequelize = require('Sequelize');
+const Sequelize = require('sequelize');
 const db = require('../config/database');
 const Proyectos = require('../models/Proyectos');
 const bcrypt = require('bcrypt-nodejs');
